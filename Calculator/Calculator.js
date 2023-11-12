c9985ae16dc2da444ab7c5b14165ef69dbff9b74
slavik <116015277+masihSlavik@users.noreply.github.com>
2023-11-12 12:26:55 +0330

    Add file from script

---
+++ Calculator/Calculator.js
@@ -0,0 +1,4 @@
+let Show_return = document.getElementById('Show_Num')
+let plus = document.getElementById("plus")
+let mines = document.getElementById("mines")
+let refresh = document.getElementById("refresh")
